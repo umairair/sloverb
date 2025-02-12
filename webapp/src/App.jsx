eb51d5796d3fb492f736c5f5a9b6f3e8bcae6386
umair <umairmalik.3400@gmail.com>
2025-02-12 17:50:59 -0500

    renamed component named 'Player' to 'Edit'

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Upload from "./pages/Upload";
-import Player from "./pages/Player";
+import Edit from "./pages/Edit";
 
 function App() {
   const [currentMP3, setCurrentMP3] = useState(null);
 
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-gray-200">
-      {!currentMP3? <Upload setCurrentMP3={setCurrentMP3}/>: <Player currentMP3={currentMP3} setCurrentMP3={setCurrentMP3}/>}
+      {!currentMP3? <Upload setCurrentMP3={setCurrentMP3}/>: <Edit currentMP3={currentMP3} setCurrentMP3={setCurrentMP3}/>}
     </div>
   );
 }
